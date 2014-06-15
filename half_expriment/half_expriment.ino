24edf08e087e0cecd4cb14e7d9bf026a80962dda
mooophy <MoophyAlan@gmail.com>
2014-06-15 15:59:58 +1200

    half step done! 	new file:   half_expriment/half_expriment.ino

---
+++ half_expriment/half_expriment.ino
@@ -0,0 +1,77 @@
+const int A = 8;
+const int B = 9;
+const int C = 10;
+const int D = 11;
+
+int count = 12;
+
+// speed at 20-500
+const unsigned width = 20;
+
+void setup() {                
+  pinMode(A, OUTPUT);     
+  pinMode(B, OUTPUT);     
+  pinMode(C, OUTPUT);     
+  pinMode(D, OUTPUT);     
+}
+
+void loop() {
+
+  while(count > 0)
+  {
+  digitalWrite(A, HIGH);
+  digitalWrite(B, LOW);
+  digitalWrite(C, HIGH);
+  digitalWrite(D, LOW);
+  delay(width);
+  
+  digitalWrite(A, HIGH);
+  digitalWrite(B, LOW);
+  digitalWrite(C, LOW);
+  digitalWrite(D, LOW);
+  delay(width);
+
+  digitalWrite(A, HIGH);
+  digitalWrite(B, LOW);
+  digitalWrite(C, LOW);
+  digitalWrite(D, HIGH);
+  delay(width);
+  
+  digitalWrite(A, LOW);
+  digitalWrite(B, LOW);
+  digitalWrite(C, LOW);
+  digitalWrite(D, HIGH);
+  delay(width);
+
+  digitalWrite(A, LOW);
+  digitalWrite(B, HIGH);
+  digitalWrite(C, LOW);
+  digitalWrite(D, HIGH);
+  delay(width);
+  
+  digitalWrite(A, LOW);
+  digitalWrite(B, HIGH);
+  digitalWrite(C, LOW);
+  digitalWrite(D, LOW);
+  delay(width);
+  
+  digitalWrite(A, LOW);
+  digitalWrite(B, HIGH);
+  digitalWrite(C, HIGH);
+  digitalWrite(D, LOW);
+  delay(width);
+  
+  digitalWrite(A, LOW);
+  digitalWrite(B, LOW);
+  digitalWrite(C, HIGH);
+  digitalWrite(D, LOW);
+  delay(width);
+  
+  --count;
+}
+  delay(3000);
+  count = 12;
+
+}
+
+
