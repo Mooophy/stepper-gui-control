a3c6fc9f443520c2684343fff78c6968b05a3700
mooophy <MoophyAlan@gmail.com>
2014-06-16 14:52:15 +1200

    modified:   PWM_generation_expriment/PWM_generation_expriment.ino

--- PWM_generation_expriment/PWM_generation_expriment.ino
+++ PWM_generation_expriment/PWM_generation_expriment.ino
@@ -1,15 +1,21 @@
 const int pwm = 13;
 
-int count = 12;
-
 
 void setup() {                
   pinMode(pwm, OUTPUT);     
 }
 
+void generate(unsigned pin, unsigned period, unsigned duty);
+
 void loop() {
-
-
+  generate(pwm,100,80);
 }
-
+void generate(unsigned pin, unsigned period, unsigned duty)
+{
+  digitalWrite(pin, HIGH);
+  delay(duty);
+  
+  digitalWrite(pin, LOW);
+  delay(period - duty);  
+}
 
