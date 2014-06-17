189f368038fdbf3b4651b3915c6740e4298daf45
mooophy <MoophyAlan@gmail.com>
2014-06-17 14:56:22 +1200

    modified:   serial_experiment/serial_experiment.ino

--- serial_experiment/serial_experiment.ino
+++ serial_experiment/serial_experiment.ino
@@ -1,8 +1,18 @@
-void setup(){
-  Serial.begin(9600);
+int incomingByte = 0;   // for incoming serial data
+
+void setup() {
+        Serial.begin(9600);     // opens serial port, sets data rate to 9600 bps
 }
 
-void loop()
-{
-  Serial.write("84\r\n"); // send a byte with the value 45
+void loop() {
+
+        // send data only when you receive data:
+        if (Serial.available() > 0) {
+                // read the incoming byte:
+                incomingByte = Serial.read();
+
+                // say what you got:
+                Serial.print("I received: ");
+                Serial.println(incomingByte, DEC);
+        }
 }
