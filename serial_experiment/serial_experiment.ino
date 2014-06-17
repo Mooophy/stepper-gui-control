c4b2d811770868ac952f060e61fd9201c62c344f
mooophy <MoophyAlan@gmail.com>
2014-06-17 17:08:23 +1200

    modified:   serial_experiment/serial_experiment.ino

--- serial_experiment/serial_experiment.ino
+++ serial_experiment/serial_experiment.ino
@@ -1,30 +1,27 @@
-
-
-
 void setup() {
         Serial.begin(9600);     // opens serial port, sets data rate to 9600 bps
         pinMode(13, HIGH);   
 }
 
-void getString(int arr[]);
 
 void loop() {
   
-  int container[6] = {0,0,0,0,0,0};
-      
-  getString(container);
-  
-  
 
-}
-
-void getString(int arr[])
-{
-  if(Serial.available() > 0)
+  String cmd;
+  while (Serial.available()) 
   {
-    
+    delay(3);  //delay to allow buffer to fill 
+    if (Serial.available() > 0) 
+    {
+      char c = Serial.read();  //gets one byte from serial buffer
+      cmd += c; //makes the string readString
+    } 
+  }  
+  
+  if(cmd.length() > 0) 
+  {
+    Serial.println(cmd); //see what was received
   }
-  
-  
-  
 }
+
+
