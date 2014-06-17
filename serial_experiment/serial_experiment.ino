bab497b2fad3f1966fff02032165342b07d09dfc
mooophy <MoophyAlan@gmail.com>
2014-06-17 17:04:40 +1200

    tested uart between PC and arduino 	modified:   serial_experiment/serial_experiment.ino

--- serial_experiment/serial_experiment.ino
+++ serial_experiment/serial_experiment.ino
@@ -1,18 +1,30 @@
-int incomingByte = 0;   // for incoming serial data
+
+
 
 void setup() {
         Serial.begin(9600);     // opens serial port, sets data rate to 9600 bps
+        pinMode(13, HIGH);   
 }
 
+void getString(int arr[]);
+
 void loop() {
+  
+  int container[6] = {0,0,0,0,0,0};
+      
+  getString(container);
+  
+  
 
-        // send data only when you receive data:
-        if (Serial.available() > 0) {
-                // read the incoming byte:
-                incomingByte = Serial.read();
+}
 
-                // say what you got:
-                Serial.print("I received: ");
-                Serial.println(incomingByte, DEC);
-        }
+void getString(int arr[])
+{
+  if(Serial.available() > 0)
+  {
+    
+  }
+  
+  
+  
 }
